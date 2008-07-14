1aa9e87e6068ef5aa15c7bfb5e833f59fccbd246
bdamage <bdamage@f8d4d631-fb50-0410-a5e1-caf36a277498>
2008-07-14 20:35:04 +0000

    git-svn-id: http://svn.code.sf.net/p/gamescanner/code/trunk@48 f8d4d631-fb50-0410-a5e1-caf36a277498

--- version-1.01/GameScanner/GameScanner/dde_wrapper.h
+++
@@ -1,7 +0,0 @@
-
-
-#include "structs_defines.h"
-
-void DDE_DeInit();
-void DDE_DeInit();
-BOOL DDE_Send(char * szMsg);
--- version-1.01/GameScanner/GameScanner/steam.h
+++
@@ -1,9 +0,0 @@
-#include "GameScanner.h"
-#include "structs_defines.h"
-#include "utilz.h"
-
-void STEAM_OnServerSelection(SERVER_INFO* pServerInfo,long (*UpdatePlayerListView)(PLAYERDATA *Q3players),long (*UpdateRulesList)(SERVER_RULES*pServer_Rules) );
-DWORD STEAM_parseServers(char * packet, DWORD length, GAME_INFO *pGI,char *szLastIP,DWORD &dwLastPort);
-DWORD STEAM_ConnectToMasterServer(GAME_INFO *pGI);
-DWORD STEAM_Get_ServerStatus(SERVER_INFO *pSI,long (*UpdatePlayerListView)(PLAYERDATA *Q3players),long (*UpdateRulesListView)(SERVER_RULES *pServerRules));
-void STEAM_SetCallbacks(long (*UpdateServerListView)(DWORD index), long (*_Callback_CheckForBuddy)(PLAYERDATA *pPlayers, SERVER_INFO* pServerInfo),long (*InsertServerItem)(GAME_INFO *pGI,SERVER_INFO pSI));
