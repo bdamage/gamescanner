25c074090c65c614c492836215405692b8c8f3b2
bdamage <bdamage@f8d4d631-fb50-0410-a5e1-caf36a277498>
2009-07-24 19:42:50 +0000

    git-svn-id: http://svn.code.sf.net/p/gamescanner/code/trunk@400 f8d4d631-fb50-0410-a5e1-caf36a277498

--- GameScanner/GameScanner/unreal.h
+++
@@ -1,6 +0,0 @@
-
-
-
-DWORD UT_Get_ServerStatus(SERVER_INFO *pSI,long (*UpdatePlayerListView)(PLAYERDATA *pPlayers),long (*UpdateRulesListView)(SERVER_RULES *pServerRules));
-char *UT_ParseServerRules(SERVER_RULES* &pLinkedListStart,char *packet,DWORD packetlen);
-PLAYERDATA *UT_ParsePlayers(SERVER_INFO *pSI,char *pointer,char *end, DWORD *numPlayers);
--- redist/readme.txt
+++
@@ -1,8 +0,0 @@
-Replace your newly compiled exe and dll's into this folder..
-Ensure to update HttpFileDonwload.dll and IPtoCountry.dll...
-
-
-Or you can move these files into your Debug or Release folder under GameScanner.
-
-
-
